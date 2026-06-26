28eba22fb35cf522443de7c7f2ca31bc66f0c704
Michael <michaelivancich26@gmail.com>
2026-06-26 15:11:26 -0400

    Move Knowledge Graph button above channels in sidebar

--- client/components/Sidebar.tsx
+++ client/components/Sidebar.tsx
@@ -111,6 +111,18 @@ export default function Sidebar({ activeRoomName }: Props) {
       {/* Nav */}
       <nav className="flex-1 overflow-y-auto overflow-x-hidden py-2">
 
+        {/* Knowledge Graph */}
+        <div className="mb-2 px-2">
+          <button onClick={() => router.push("/graph")}
+            className={`flex w-full items-center gap-2 rounded-lg px-2 py-1.5 text-gray-500 hover:bg-gray-800 hover:text-amber-400 transition-colors ${collapsed ? "justify-center" : ""}`}
+            title="Knowledge Graph">
+            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" className="h-4 w-4 shrink-0">
+              <path fillRule="evenodd" d="M10 1a9 9 0 1 0 0 18A9 9 0 0 0 10 1ZM4.5 9.5a.75.75 0 0 0 0 1.5h3.69l-1.22 1.22a.75.75 0 1 0 1.06 1.06l2.5-2.5a.75.75 0 0 0 0-1.06l-2.5-2.5a.75.75 0 0 0-1.06 1.06l1.22 1.22H4.5Zm6.25-3.25a.75.75 0 0 1 .75-.75h.5a3 3 0 0 1 0 6h-.5a.75.75 0 0 1 0-1.5h.5a1.5 1.5 0 0 0 0-3h-.5a.75.75 0 0 1-.75-.75Z" clipRule="evenodd" />
+            </svg>
+            {!collapsed && <span className="text-sm">Knowledge Graph</span>}
+          </button>
+        </div>
+
         {/* Rooms */}
         <div className="mb-1">
           {collapsed ? (
@@ -232,19 +244,6 @@ export default function Sidebar({ activeRoomName }: Props) {
           )}
         </div>
       </nav>
-
-      {/* Graph link */}
-      <div className="border-t border-gray-800 p-2">
-        <button onClick={() => router.push("/graph")}
-          className={`flex w-full items-center gap-2 rounded-lg px-2 py-2 text-gray-500 hover:bg-gray-800 hover:text-amber-400 transition-colors ${collapsed ? "justify-center" : ""}`}
-          title="Knowledge Graph">
-          <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" className="h-4 w-4 shrink-0">
-            <path d="M13.024 9.25c.47 0 .827-.433.637-.863a4 4 0 0 0-4.094-2.364c-.468.05-.665.576-.43.984l.208.356a.75.75 0 0 0 .84.336 2.5 2.5 0 0 1 2.396.95c.209.28.582.41.443-.402Z" />
-            <path fillRule="evenodd" d="M10 2a8 8 0 1 0 0 16A8 8 0 0 0 10 2ZM4 10a6 6 0 1 1 12 0A6 6 0 0 1 4 10Z" clipRule="evenodd" />
-          </svg>
-          {!collapsed && <span className="text-sm">Knowledge Graph</span>}
-        </button>
-      </div>
 
       {/* Sign out */}
       <div className="border-t border-gray-800 p-2">
